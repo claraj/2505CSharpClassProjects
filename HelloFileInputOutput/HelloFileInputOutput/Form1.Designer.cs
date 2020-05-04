44ffabb402d4173ca7333fa173ea5af5fe005c78
clara <10088152+claraj@users.noreply.github.com>
2020-05-04 13:03:15 -0400

    File input/output todo item example

---
+++ HelloFileInputOutput/HelloFileInputOutput/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace HelloFileInputOutput
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNewToDo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddTodo = new System.Windows.Forms.Button();
+            this.lstToDo = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNewToDo
+            // 
+            this.txtNewToDo.Location = new System.Drawing.Point(182, 34);
+            this.txtNewToDo.Name = "txtNewToDo";
+            this.txtNewToDo.Size = new System.Drawing.Size(354, 26);
+            this.txtNewToDo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter To do Item";
+            // 
+            // btnAddTodo
+            // 
+            this.btnAddTodo.Location = new System.Drawing.Point(461, 77);
+            this.btnAddTodo.Name = "btnAddTodo";
+            this.btnAddTodo.Size = new System.Drawing.Size(75, 36);
+            this.btnAddTodo.TabIndex = 4;
+            this.btnAddTodo.Text = "Add";
+            this.btnAddTodo.UseVisualStyleBackColor = true;
+            this.btnAddTodo.Click += new System.EventHandler(this.btnAddTodo_Click);
+            // 
+            // lstToDo
+            // 
+            this.lstToDo.FormattingEnabled = true;
+            this.lstToDo.ItemHeight = 20;
+            this.lstToDo.Location = new System.Drawing.Point(52, 135);
+            this.lstToDo.Name = "lstToDo";
+            this.lstToDo.Size = new System.Drawing.Size(484, 264);
+            this.lstToDo.TabIndex = 5;
+            this.lstToDo.SelectedIndexChanged += new System.EventHandler(this.lstToDo_SelectedIndexChanged);
+            this.lstToDo.KeyUp += new System.Windows.Forms.KeyEventHandler(this.lstToDo_DeleteKeyPressed);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(309, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Select an item and press Delete to remove";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAddTodo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 479);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstToDo);
+            this.Controls.Add(this.btnAddTodo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNewToDo);
+            this.Name = "Form1";
+            this.Text = "File Input and Output";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNewToDo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddTodo;
+        private System.Windows.Forms.ListBox lstToDo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HelloFileInputOutput/HelloFileInputOutput/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloFileInputOutput
+{
+    public partial class Form1 : Form
+    {
+
+        private const string ToDoFileName = @"todo_items.txt";
+        private const string SelectedIndexFileName = @"last_selected.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadToDoItems();
+            LoadLastSelectedItem();
+        }
+  
+        private void LoadToDoItems()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(ToDoFileName);
+                lstToDo.Items.Clear();
+                lstToDo.Items.AddRange(lines);
+            } 
+           catch (Exception e)
+            {
+                // errors reading from file - developer should think about how to handle these
+                // In this app, assume user has not created any todo items yet so there is no file.
+                Debug.WriteLine("Error reading todo item file, " + e, "Error");
+            }
+        }
+
+        private void LoadLastSelectedItem()
+        {
+            try
+            {
+                // expect this to be a single line with a number, representing the 
+                // index of the item selected in the ListBox
+                
+                string text = File.ReadAllText(SelectedIndexFileName);
+                text = text.Trim();  // remove any whitespace
+                int selectedIndex = Int32.Parse(text);
+
+                lstToDo.SelectedIndex = selectedIndex;
+
+            }
+            catch (Exception e) 
+            {
+                /* ignore - the user probably hasn't created any todo items yet
+                 or the data in the file is not a number 
+                 or the number is not a valid index for the ListBox.
+                 
+                In other apps, you may need to catch and handle these execptions, 
+                and possibly take different actions depending on the exception.
+                 */ 
+                Debug.WriteLine("Error reading last selected item because:" + e);
+            }   
+        }
+
+        private void btnAddTodo_Click(object sender, EventArgs e)
+        {
+            string text = txtNewToDo.Text;
+            if (!String.IsNullOrEmpty(text))
+            {
+                lstToDo.Items.Add(text);
+                SaveToDoItems();
+                SaveCurrentSelection();
+                txtNewToDo.Text = "";
+            }
+        }
+
+        private void SaveToDoItems()
+        {
+            List<string> todoItems = new List<string>();
+
+            foreach (string item in lstToDo.Items)
+            {
+                todoItems.Add(item);
+            }
+
+            string todoItemsText = String.Join(Environment.NewLine, todoItems);
+
+            try
+            {
+                // Note this method overwrites any information in the file if it already exists
+                // Look up the AppendAllText method if you want to add text to an existing file
+                File.WriteAllText(ToDoFileName, todoItemsText);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error saving to do items", "Error");
+                Debug.WriteLine("Error saving to do items because " + e);
+            }
+        }
+
+        private void SaveCurrentSelection()
+        {
+            int currentSelection = lstToDo.SelectedIndex;
+            try
+            {
+                File.WriteAllText(SelectedIndexFileName, currentSelection.ToString());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error saving selected item", "Error");
+                Debug.WriteLine("Error saving selected item index because " + e);
+            }
+        }
+
+        private void lstToDo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SaveCurrentSelection();
+        }
+
+        private void lstToDo_DeleteKeyPressed(object sender, KeyEventArgs e)
+        {
+            // If the user presses Delete or Backspace, delete the selected item
+            if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back)
+            {
+                int selectedIndex = lstToDo.SelectedIndex;
+                if (selectedIndex != -1)
+                {
+                    lstToDo.Items.RemoveAt(selectedIndex);
+                    SaveCurrentSelection();
+                    SaveToDoItems();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
